d1a1e646baf7ae04f76b958a7104221309e88fb2
pagustinamato <42480020+pagustinamato@users.noreply.github.com>
2023-07-24 21:44:14 -0300

    Correcion_V1
    Modique el .py para tomar mas datos y fecha. Cree 3 estructuras de tablas.

--- Entregable_1/CREATE TABLE.sql
+++ Entregable_1/CREATE TABLE.sql
@@ -1,8 +1,32 @@
-create table Teams_nba ( 
-id int not null primary key, 
-full_name char(30) null, 
-name char(20) null,
-abbreviation char(10) null,
-city char(30) null,
-conference char(8) null,
-division char(15) null);
+create table games_NBA( 
+id int not null primary key,
+foreign key(home_team_id) references home_teams_nba(id), 
+foreign key(visitor_team_id) references visitors_teams_nba(id),
+date TIMESTAMPTZ null, 
+home_team_score int null,
+visitor_team_score int null,
+season int null,
+periodo int null,
+status char(10) null,
+timer time null,
+postseason char(10) null,
+home_team_id int not null, 
+visitor_team_id int not null);
+
+create table home_teams_nba( 
+id int not null primary key,
+abbreviation char(5),
+city char(20),
+conference char(8),
+division char(15),
+full_Name char(30),
+short_name char(20));
+
+create table visitors_teams_nba( 
+id int not null primary key,
+abbreviation char(5),
+city char(20),
+conference char(8),
+division char(15),
+full_Name char(30),
+short_name char(20));
--- Entregable_1/Entregable _1.py
+++ Entregable_1/Entregable _1.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 
 url = 'https://www.balldontlie.io/api/v1/'
-endpoint = 'teams'
+endpoint = 'games'
 
 
 def main_request(url, endpoint):
@@ -14,27 +14,68 @@ def get_pages(response):
     return response['meta']['per_page']
 
 
-def parse_json(response):
-    teamlist = []
+def parse_json_games(response):
+    gameslist = []
     for e in response['data']:
         char = {
             'id': e['id'],
-            'full_name': e['full_name'],
-            'name': e['name'],
-            'abbreviation': e['abbreviation'],
-            'city': e['city'],
-            'conference': e['conference'],
-            'division': e['division']
+            'date': e['date'],
+            'home_team_score': e['home_team_score'],
+            'visitor_team_score': e['visitor_team_score'],
+            'season': e['season'],
+            'period': e['period'],
+            'status': e['status'],
+            'home_team_id': e['home_team']['id'],
+            'visitor_team_id': e['visitor_team']['id']
         }
-        teamlist.append(char)
-    return teamlist
+        gameslist.append(char)
+    return gameslist
+
+
+def parse_json_home_team(response):
+    home_team_list = []
+    for e in response['data']:
+        char = {
+            'id': e['home_team']['id'],
+            'abbreviation': e['home_team']['abbreviation'],
+            'city': e['home_team']['city'],
+            'conference': e['home_team']['conference'],
+            'division': e['home_team']['division'],
+            'full_name': e['home_team']['full_name'],
+            'name': e['home_team']['name']
+        }
+        home_team_list.append(char)
+    return home_team_list
+
+
+def parse_json_visitor_team(response):
+    visitor_team_list = []
+    for e in response['data']:
+        char = {
+            'id': e['visitor_team']['id'],
+            'abbreviation': e['visitor_team']['abbreviation'],
+            'city': e['visitor_team']['city'],
+            'conference': e['visitor_team']['conference'],
+            'division': e['visitor_team']['division'],
+            'full_name': e['visitor_team']['full_name'],
+            'name': e['visitor_team']['name']
+        }
+        visitor_team_list.append(char)
+    return visitor_team_list
 
 
 data = main_request(url, endpoint)
 
-df = pd.DataFrame(parse_json(data))
+df_games = pd.DataFrame(parse_json_games(data))
+
+df_home_team = pd.DataFrame(parse_json_home_team(data))
+
+df_visitor_team = pd.DataFrame(parse_json_visitor_team(data))
 
 from sqlalchemy import create_engine
-conn = create_engine('postgresql://pagustinamato_coderhouse:7kxDU66Urk@data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com:5439/data-engineer-database')
-df.to_sql('teams_nba', conn, index=False, schema='pagustinamato_coderhouse', if_exists='replace')
 
+conn = create_engine(
+    'postgresql://pagustinamato_coderhouse:7kxDU66Urk@data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com:5439/data-engineer-database')
+df_games.to_sql('games_nba', conn, index=False, schema='pagustinamato_coderhouse', if_exists='replace', method=None)
+df_home_team.to_sql('home_teams_nba', conn, index=False, schema='pagustinamato_coderhouse', if_exists='replace', method=None)
+df_visitor_team.to_sql('visitors_teams_nba', conn, index=False, schema='pagustinamato_coderhouse', if_exists='replace', method=None)
